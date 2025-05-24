8d21a6f77f1eb6ab514b0af7924af197deaad63b
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-05-24 20:42:53 -0300

    :package: build: contruindo a Classe do Filme

---
+++ src/main/java/rocket/Filme.java
@@ -0,0 +1,11 @@
+package rocket;
+
+public class Filme {
+     
+     String titulo;
+     int anoLancamento;
+     boolean incluidoNoPlano;
+     double avaliacao;
+     int duracaoEmMinutos;
+     int totalDeAvaliacoes;
+}
