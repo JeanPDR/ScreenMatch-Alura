077da1030d8793fd597e44ade54e33ff562d5f91
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-05-24 21:51:09 -0300

    :truck: chore: Definindo ações para o Filme com mais Métodos

--- src/main/java/rocket/Filme.java
+++ src/main/java/rocket/Filme.java
@@ -5,7 +5,29 @@ public class Filme {
      String titulo;
      int anoLancamento;
      boolean incluidoNoPlano;
-     double avaliacao;
+     double somaDeAvaliacoes;
      int duracaoEmMinutos;
      int totalDeAvaliacoes;
+
+     //Método para exibir a ficha técnica do filme
+     // O método exibeFichaTecnica() imprime as informações do filme no console.
+
+     void exibeFichaTecnica() {
+         System.out.println("Título: " + titulo);
+         System.out.println("Ano de Lançamento: " + anoLancamento);
+         System.out.println("Incluído no Plano: " + incluidoNoPlano);
+         System.out.println("Avaliação: " + somaDeAvaliacoes);
+         System.out.println("Duração em Minutos: " + duracaoEmMinutos);
+         System.out.println("Total de Avaliações: " + totalDeAvaliacoes);
+     }
+
+     void avaliacao (double nota) {
+          somaDeAvaliacoes += nota;
+          totalDeAvaliacoes++;
+     }
+
+     double pegaMedia() {
+          return somaDeAvaliacoes / totalDeAvaliacoes;
+     }
 }
+
--- src/main/java/rocket/Principal.java
+++ src/main/java/rocket/Principal.java
@@ -5,8 +5,17 @@ public class Principal {
         Filme meuFilme = new Filme();
         meuFilme.titulo = "A Origem";
         meuFilme.anoLancamento = 2010;
-        meuFilme.incluidoNoPlano = true;
-        meuFilme.avaliacao = 8.8;
+        meuFilme.incluidoNoPlano = true; 
         meuFilme.duracaoEmMinutos = 148;
+
+        meuFilme.exibeFichaTecnica();
+        // Exibe a ficha técnica do filme
+        // O método exibeFichaTecnica() imprime as informações do filme no console.
+        meuFilme.avaliacao(5.0);
+        meuFilme.avaliacao(4.5);
+
+        System.out.println(meuFilme.somaDeAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
