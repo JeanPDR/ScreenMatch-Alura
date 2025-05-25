0ff4541e4498c2fdcdcafa3947ce9ec1966e8523
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-05-24 21:18:19 -0300

    :recycle: refactor: Alterando a Classe Main para Principal e atribuindo valores ao filme

---
+++ src/main/java/rocket/Principal.java
@@ -0,0 +1,12 @@
+package rocket;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.titulo = "A Origem";
+        meuFilme.anoLancamento = 2010;
+        meuFilme.incluidoNoPlano = true;
+        meuFilme.avaliacao = 8.8;
+        meuFilme.duracaoEmMinutos = 148;
+    }
+}
