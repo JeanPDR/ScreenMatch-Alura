0b305d21567be300b52b11ecbe5e68c24344a070
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-10-07 20:34:10 -0300

    :tada: trabalhando com heranças

--- src/main/java/rocket/Filme.java
+++ src/main/java/rocket/Filme.java
@@ -1,33 +1,68 @@
 package rocket;
 
 public class Filme {
-     
-     String titulo;
-     int anoLancamento;
-     boolean incluidoNoPlano;
-     double somaDeAvaliacoes;
-     int duracaoEmMinutos;
-     int totalDeAvaliacoes;
 
-     //Método para exibir a ficha técnica do filme
-     // O método exibeFichaTecnica() imprime as informações do filme no console.
+    private String titulo;
+    private int anoLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDeAvaliacoes;
+    private int duracaoEmMinutos;
+    private int totalDeAvaliacoes;
 
-     void exibeFichaTecnica() {
-         System.out.println("Título: " + titulo);
-         System.out.println("Ano de Lançamento: " + anoLancamento);
-         System.out.println("Incluído no Plano: " + incluidoNoPlano);
-         System.out.println("Avaliação: " + somaDeAvaliacoes);
-         System.out.println("Duração em Minutos: " + duracaoEmMinutos);
-         System.out.println("Total de Avaliações: " + totalDeAvaliacoes);
-     }
+    //Método para exibir a ficha técnica do filme
+    // O método exibeFichaTecnica() imprime as informações do filme no console.
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
 
-     void avaliacao (double nota) {
-          somaDeAvaliacoes += nota;
-          totalDeAvaliacoes++;
-     }
+    public String getTitulo() {
+        return titulo;
+    }
 
-     double pegaMedia() {
-          return somaDeAvaliacoes / totalDeAvaliacoes;
-     }
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Ano de Lançamento: " + anoLancamento);
+        System.out.println("Incluído no Plano: " + incluidoNoPlano);
+        System.out.println("Avaliação: " + somaDeAvaliacoes);
+        System.out.println("Duração em Minutos: " + duracaoEmMinutos);
+        System.out.println("Total de Avaliações: " + totalDeAvaliacoes);
+    }
+
+    public void avaliacao(double nota) {
+        somaDeAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        return somaDeAvaliacoes / totalDeAvaliacoes;
+    }
+
 }
-
--- src/main/java/rocket/Principal.java
+++ src/main/java/rocket/Principal.java
@@ -1,12 +1,13 @@
 package rocket;
 
 public class Principal {
+
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.titulo = "A Origem";
-        meuFilme.anoLancamento = 2010;
-        meuFilme.incluidoNoPlano = true; 
-        meuFilme.duracaoEmMinutos = 148;
+        meuFilme.setTitulo("A Origem");
+        meuFilme.setAnoLancamento(2010);
+        meuFilme.setIncluidoNoPlano(true);
+        meuFilme.setDuracaoEmMinutos(148);
 
         meuFilme.exibeFichaTecnica();
         // Exibe a ficha técnica do filme
@@ -14,8 +15,7 @@ public class Principal {
         meuFilme.avaliacao(5.0);
         meuFilme.avaliacao(4.5);
 
-        System.out.println(meuFilme.somaDeAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
